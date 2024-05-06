1a7dd14bef69891753025590962b2ce3c6ae261e
CETECOM <cetecom_conce@duoc.cl>
2024-05-06 18:26:16 -0400

    primer avance

---
+++ plantillainicial/furni-1.0.0/js/validacion.js
@@ -0,0 +1,21 @@
+const formulario = document.getElementById('formulario');
+
+const primer_nombre = document.getElementById('primer_nombre');
+const segundo_nombre = document.getElementById('segundo_nombre');
+const apellido = document.getElementById('apellido');
+const direccion = document.getElementById('direccion');
+const correo = document.getElementById('correo');
+const celular = document.getElementById('celular');
+const adicional = document.getElementById('adicional');
+const direccion2 = document.getElementById('direccion2');
+
+const expresiones = {
+    primer_nombre:
+    segundo_nombre:
+    apellido:
+    direccion:
+    correo: /^[a-zA-Z0-9_.+-]
+    celular: /^\d{9}$/,
+    adicional:
+    direccion2:
+}
